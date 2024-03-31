6a663c2f2359a8cb0fb9f6360053db3678550d76
Amruth <ammumandappa@gmail.com>
2024-03-31 17:36:00 +0530

    storing in an array

--- todo-list.js
+++ todo-list.js
@@ -1,10 +1,12 @@
-const todoList = []
+const todoList = [];
 
-function addTodo(){
-    const inputElement = document.querySelector('.js-name-input')
-    const  name = inputElement.value
-    console.log(name)
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+  console.log(name);
 
-    todoList.push(name)
-    console.log(todoList)
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = "";
 }
